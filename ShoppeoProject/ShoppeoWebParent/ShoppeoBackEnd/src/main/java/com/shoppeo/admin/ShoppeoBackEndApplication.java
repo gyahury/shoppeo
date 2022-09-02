5413b5ac297067001beb8362139ff5db12530036
rudgn <rudgn@DESKTOP-3PODPS4>
2022-09-02 18:20:00 +0900

    shoppeo 기본 구조 등록

---
+++ ShoppeoProject/ShoppeoWebParent/ShoppeoBackEnd/src/main/java/com/shoppeo/admin/ShoppeoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shoppeo.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppeoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppeoBackEndApplication.class, args);
+	}
+
+}
---
+++ ShoppeoProject/ShoppeoWebParent/ShoppeoFrontEnd/src/main/java/com/shoppeo/site/ShoppeoFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shoppeo.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppeoFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppeoFrontEndApplication.class, args);
+	}
+
+}
