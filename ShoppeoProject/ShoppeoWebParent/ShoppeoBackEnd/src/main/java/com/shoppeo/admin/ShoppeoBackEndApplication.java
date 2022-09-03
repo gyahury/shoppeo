16ba6d8b2cb25064a6a66217fe389bc046a2da30
gyahury <rudgns1243@naver.com>
2022-09-03 23:29:41 +0900

    commit test

--- ShoppeoProject/ShoppeoWebParent/ShoppeoBackEnd/src/main/java/com/shoppeo/admin/ShoppeoBackEndApplication.java
+++ ShoppeoProject/ShoppeoWebParent/ShoppeoBackEnd/src/main/java/com/shoppeo/admin/ShoppeoBackEndApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppeoBackEndApplication {
+public class ShoppeoBackEndApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppeoBackEndApplication.class, args);
