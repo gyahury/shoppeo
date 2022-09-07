0cbe44c9410035e00078de986049a4aa0761e4c5
gyahury <rudgns1243@naver.com>
2022-09-07 17:58:57 +0900

    role 엔티티 테스트 작성 중 저장

--- ShoppeoProject/ShoppeoWebParent/ShoppeoBackEnd/src/test/java/com/shoppeo/admin/user/RoleRepositoryTests.java
+++ ShoppeoProject/ShoppeoWebParent/ShoppeoBackEnd/src/test/java/com/shoppeo/admin/user/RoleRepositoryTests.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import com.shoppeo.common.entity.Role;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 public class RoleRepositoryTests {
 	
 	@Autowired
@@ -21,5 +23,9 @@ public class RoleRepositoryTests {
 		Role roleAdmin = new Role("admin","manage Everything");
 		Role savedRole= repo.save(roleAdmin);
 		assertThat(savedRole.getId()).isGreaterThan(0);
+	}
+	@Test
+	public void testCreateRestRole() {
+		
 	}
 }
