a0e0e82672cedb72acb78bc0f868edfe213b922e
gyahury <rudgns1243@naver.com>
2022-09-08 15:32:33 +0900

    Role테이블 엔티티 작성 테스트

--- ShoppeoProject/ShoppeoWebParent/ShoppeoBackEnd/src/test/java/com/shoppeo/admin/user/RoleRepositoryTests.java
+++ ShoppeoProject/ShoppeoWebParent/ShoppeoBackEnd/src/test/java/com/shoppeo/admin/user/RoleRepositoryTests.java
@@ -2,6 +2,8 @@ package com.shoppeo.admin.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
@@ -26,6 +28,14 @@ public class RoleRepositoryTests {
 	}
 	@Test
 	public void testCreateRestRole() {
+		Role roleSalesperson = new Role("Salesperson","상품 가격, 고객, 배송, 주문, 통계 관리");
 		
+		Role roleEditor = new Role("Editor","카테고리, 브랜드, 상품, 메뉴 및 아티클 관리");
+		
+		Role roleShipper = new Role("Shipper","상품, 주문 조회 및 주문 상태 관리");
+		
+		Role roleAssistant = new Role("Assistant","문의, 리뷰 관리");
+		
+		repo.saveAll(List.of(roleSalesperson,roleEditor,roleShipper,roleAssistant));
 	}
 }
